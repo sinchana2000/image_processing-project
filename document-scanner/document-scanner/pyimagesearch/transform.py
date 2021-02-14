b93a693afdde95ff3db13566ca56bbad9ab78eca
sinchana2000 <46958639+sinchana2000@users.noreply.github.com>
2021-02-14 14:44:07 +0530

    Update transform.py

--- document-scanner/document-scanner/pyimagesearch/transform.py
+++ document-scanner/document-scanner/pyimagesearch/transform.py
@@ -62,3 +62,49 @@ def four_point_transform(image, pts):
 
 	# return the warped image
 	return warped
+
+
+
+# import the necessary packages
+from pyimagesearch.transform import four_point_transform
+import numpy as np
+import argparse
+import cv2
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", help = "path to the image file")
+ap.add_argument("-c", "--coords",
+	help = "comma seperated list of source points")
+args = vars(ap.parse_args())
+
+
+# load the image and grab the source coordinates (i.e. the list of
+# of (x, y) points)
+image = cv2.imread(args["image"])
+pts = np.array(eval(args["coords"]), dtype = "float32")
+
+
+# apply the four point tranform to obtain a "birds eye view" of
+# the image
+warped = four_point_transform(image, pts)
+
+# show the original and warped images
+cv2.imshow("Original", image)
+cv2.imshow("Warped", warped)
+cv2.waitKey(0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
